e53fee93423b04ada51e40864afec977b87124a0
Rayner Kovachevich <kovachevich92@gmail.com>
2025-09-02 15:39:28 +0300

    Update database connection and session setup

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5434/fastapi_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5434/fastapi_db")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
